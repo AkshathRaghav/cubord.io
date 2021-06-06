4f583e71cb3679159edcb3a5ce6b4d3e27691c5b
Akshath Raghav <akshathraghav.r@gmail.com>
2021-06-06 05:31:10 +0530

    Update Bot.java

--- src/Bot.java
+++ src/Bot.java
@@ -7,14 +7,12 @@ import javax.security.auth.login.LoginException;
 
 public class Bot {
     public static JDA jda ;
-    public static String prefix = ">cube" ;
+    public static String prefix = "-c" ;
     public static void main(String[] args) throws LoginException {
-        jda = JDABuilder.createDefault("Insert Token Here") .build();
+        jda = JDABuilder.createDefault("ODQ3MTExNDEwNjE3Njc5ODk0.YK5T-g.X0Madg30Dr2_dOvolYI2-EKevoQ").build();
         jda.getPresence().setStatus(OnlineStatus.DO_NOT_DISTURB);
         jda.getPresence().setActivity(Activity.playing("with Cubes for you"));
         jda.addEventListener(new Commands());
-        jda.addEventListener(new MakeCube());
-//        jda.addEventListener(new Botmoves());
-
+        jda.addEventListener(new CubeCommands());
     }
 }
