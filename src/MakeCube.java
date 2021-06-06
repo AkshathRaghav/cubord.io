f0e9b3bc331f4df348b3d5cdb3dee55da606a9f1
Akshath Raghav <akshathraghav.r@gmail.com>
2021-06-06 05:31:49 +0530

    Update MakeCube.java

--- src/MakeCube.java
+++ src/MakeCube.java
@@ -2,7 +2,7 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class MakeCube extends ListenerAdapter {
+public class CubeCommands extends ListenerAdapter {
     String[] temp3 = {"RRRRRRRRR", "OOOOOOOOO", "WWBWWBWWB", "YBBYBBYBB", "GGWGGWGGW", "YYGYYGYYG"};
     public  Cubot cube =  new Cubot(temp3) ;
         // RRRRRRRRR GGGGGGGGG OOOOOOOOO BBBBBBBBB WWWWWWWWW YYYYYYYYY
@@ -12,7 +12,7 @@ public class MakeCube extends ListenerAdapter {
         String content = event.getMessage().getContentRaw() ;
         String[] arg = event.getMessage().getContentRaw().split(" ");
         if (arg.length == 8 && (arg[0] + arg[1]).equalsIgnoreCase(Bot.prefix + "make")) {
-            arg = new String[]{arg[2], arg[3], arg[4], arg[5], arg[6], arg[7]};
+            arg = new String[]{arg[2].substring(1), arg[3], arg[4], arg[5], arg[6], arg[7]};
             boolean check = true;
             for (String i : arg) {
                 if (i.length() != 9) {
@@ -22,14 +22,13 @@ public class MakeCube extends ListenerAdapter {
             }
             if (check) {
                 cube = new Cubot(arg);
-//                if (!cube.isValid()) {
-//                    cube = null;
-//                    event.getChannel().sendMessage("Hey " + event.getMember().getAsMention() + ", you have entered the cube incorrectly ( Wrong colors )").queue();
-//                } else {
-//                    if ( cube.isSolved() ) { solvedatfirst = true ; }
-//
-//                    event.getChannel().sendMessage("Hey " + event.getMember().getAsMention() + ",  I've made a cube for you!").queue();
-//                }
+                if (!cube.isValid()) {
+                    cube = null;
+                    event.getChannel().sendMessage("Hey " + event.getMember().getAsMention() + ", you have entered the cube incorrectly ( Wrong colors )").queue();
+                } else {
+                    if ( cube.isSolved() ) { solvedatfirst = true ; }
+                    event.getChannel().sendMessage("Hey " + event.getMember().getAsMention() + ",  I've made a cube for you!").queue();
+                }
             }
             else {
                 event.getChannel().sendMessage("Hey " + event.getMember().getEffectiveName() + ", your cubearray is wrong man.").queue();
@@ -40,36 +39,12 @@ public class MakeCube extends ListenerAdapter {
             EmbedBuilder info = new EmbedBuilder();
             info.setColor(0Xa80d2c);
                 String temp = cube.toString(), cubes = "", temp2 = "";
-//                while (temp.contains("| Y")) {
-//                    temp2 = temp.substring(0, temp.indexOf("| Y"));
-//                    cubes += "\t\t" + temp2 ;
-//                    temp = temp.substring(temp.indexOf("| Y"));
-//                }
-//                cubes += temp.substring(0, temp.indexOf("B |")) ;
-//                while (temp.contains("| W")) {
-//                    temp2 = temp.substring(0, temp.indexOf("| W"));
-//                    cubes += "  " + temp.substring(0, temp.indexOf("| W"));
-//                    temp = temp.substring(temp.indexOf("| W"));
-//                }
-
-//            event.getChannel().sendMessage("      " + temp + event.getMember().getAsMention()).queue();
-//            String cube1 = temp.substring(0, temp.indexOf("| Y"));
-////            String cube2 = temp.substring(temp.indexOf("Middle"), temp.indexOf("Right"));
-////            String cube3 = temp.substring(temp.indexOf("Right"));
             info.addField(event.getMember().getEffectiveName() + "'s Cube",  "```" +   temp + "```\n", false);
-////            info.addBlankField(true);
-////
-////            info.addField("", cube2, false);
-////            info.addBlankField(true);
-////
-////            info.addField("", cube3, false);
-////            info.addBlankField(true);
-//
-//            info.setFooter("<--" + , event.getMember().getUser().getAvatarUrl());
+
             event.getChannel().sendMessage(info.build()).queue();
             info.clear();
         }
-        else if (arg.length > 1 && (arg[0] + arg[1]).equalsIgnoreCase(Bot.prefix + "Alg") ) {
+        else if (arg.length > 1 && (arg[0] + arg[1]).equalsIgnoreCase(Bot.prefix + "do") ) {
             if ( arg[arg.length-1].substring(arg[arg.length-1].length()-1).equals(")")) {
                 if (cube != null) {
                         String s = cube.stringalg(content.substring(content.indexOf("(") + 1, content.indexOf(")")));
@@ -81,8 +56,6 @@ public class MakeCube extends ListenerAdapter {
             }
             else {
                 event.getChannel().sendMessage("Add an alg for me to execute!"   + event.getMember().getAsMention()).queue();
-
-
             }
 
         }
@@ -106,6 +79,23 @@ public class MakeCube extends ListenerAdapter {
                     s = "Solved" ; }
                 else { s = "Not Solved" ; }
                 event.getChannel().sendMessage("Your Cube is " + s + ", " + event.getMember().getAsMention()).queue();
+
+            } else {
+                event.getChannel().sendMessage("Make a cube first " + event.getMember().getAsMention()).queue();
+            }
+        }
+        else if ( arg.length > 2 && (arg[0] + arg[1]).equalsIgnoreCase(Bot.prefix + "scramble")  ) {
+            boolean doScramble = false , check = true;
+            if (arg[3].substring(arg[3].length()-1).equals("s")) { doScramble = true ; }
+            else if (!arg[3].substring(arg[3].length()-1).equals("n")){  check = false ; }
+            if (cube != null ) {
+                if (check) {
+                    String s = cube.getScramble(Integer.parseInt(arg[2].substring(1)), doScramble);
+                    event.getChannel().sendMessage("Your scramble's here - " + s + ", " + event.getMember().getAsMention()).queue();
+                }
+                else {
+                    event.getChannel().sendMessage("Re-enter with valid values " + event.getMember().getAsMention()).queue();
+                }
 
             } else {
                 event.getChannel().sendMessage("Make a cube first " + event.getMember().getAsMention()).queue();
