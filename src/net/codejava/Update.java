ae7e1387b10e6d412c14eb041fce5ffd6b5237e4
Akshath Raghav <akshathraghav.r@gmail.com>
2021-06-18 17:28:58 +0530

    Update Update.java

--- src/net/codejava/Update.java
+++ src/net/codejava/Update.java
@@ -11,53 +11,99 @@ public class Update {
         String password = "Akshath$$123" ;
         try {
             Connection conn = DriverManager.getConnection(jdbc, username, password);
-            String sql = "SELECT name,cube "
-                    + "FROM cubes "
-                    + "WHERE name = ?";
-            PreparedStatement statement = conn.prepareStatement(sql) ;
-            statement.setString(1, "Akshath");
-            ResultSet result = statement.executeQuery() ;
+            final String queryCheck = "SELECT * from cubes WHERE name = ?";
+            final PreparedStatement ps = conn.prepareStatement(queryCheck);
+            ps.setString(1, name);
+            ResultSet resultSet = ps.executeQuery();
+            if(resultSet.next()) {
+                String sql = "SELECT name,cube "
+                        + "FROM cubes "
+                        + "WHERE name = ?";
+                PreparedStatement statement = conn.prepareStatement(sql);
+                statement.setString(1, name);
+                ResultSet result = statement.executeQuery();
 
-            while (result.next()) {
-                s = result.getString("cube");
+                while (result.next()) {
+                    s = result.getString("cube");
 
+                }
             }
-
+            else { return "Error" ; }
             conn.close();
-
         }
         catch (SQLException e) {
-            System.out.println("Error while connecting");
-            e.printStackTrace();
+            return "Error" ;
         }
         return s ;
     }
-    public static  boolean setCubeSQL(String name, String cube) {
+    public static  String getStoreSQL(String name) {
+        String s = "" ;
         String jdbc = "jdbc:postgresql://localhost:5432/Cubord" ;
         String username = "postgres" ;
         String password = "Akshath$$123" ;
         try {
             Connection conn = DriverManager.getConnection(jdbc, username, password);
-            String sql = "INSERT INTO cubes (name, cube)"
-                    + " VALUES ('" + name + "', '" + cube + "')" ;
-            Statement statement = conn.createStatement() ;
-            int rows = statement.executeUpdate(sql);
-            if (!(rows>0)) {
-                return false ;
+            String sql = "SELECT name,cube,store "
+                    + "FROM cubes "
+                    + "WHERE name = ?";
+            PreparedStatement statement = conn.prepareStatement(sql) ;
+            statement.setString(1, name);
+            ResultSet result = statement.executeQuery() ;
+
+            while (result.next()) {
+                s = result.getString("store");
+
+            }
+            conn.close();
+        }
+        catch (SQLException e) {
+            return "Error" ;
+        }
+        return s ;
+    }
+    public static  boolean setCubeSQL(String name, String store,  String cube) {
+        String jdbc = "jdbc:postgresql://localhost:5432/Cubord" ;
+        String username = "postgres" ;
+        String password = "Akshath$$123" ;
+        try {
+            Connection conn = DriverManager.getConnection(jdbc, username, password);
+            final String queryCheck = "SELECT * from cubes WHERE name = ?";
+            final PreparedStatement ps = conn.prepareStatement(queryCheck);
+            ps.setString(1, name);
+            ResultSet resultSet = ps.executeQuery();
+            if(resultSet.next()) {
+                String SQL = "UPDATE cubes "
+                        + "SET cube = ?,store = ? "
+                        + "WHERE name = ?";
+                PreparedStatement pstmt = conn.prepareStatement(SQL) ;
+                pstmt.setString(1, cube);
+                pstmt.setString(2, store);
+                pstmt.setString(3, name);
+                int rows = pstmt.executeUpdate();
+            }
+            else {
+                String sql = "INSERT INTO cubes (name, cube)"
+                        + " VALUES ('" + name + "', '" + cube + "')";
+                Statement statement = conn.createStatement();
+                int rows = statement.executeUpdate(sql);
+                if (!(rows > 0)) {
+                    return false;
+                }
             }
             conn.close();
             return true ;
         }
         catch (SQLException e) {
-
+            e.printStackTrace();
             return false ;
         }
     }
-    public static void updateCubeSQL() {}
+
     public static void main(String[] args)
     {
-        System.out.println(getCubeSQL("Jha")) ;
-//        System.out.println(setCubeSQL("Jha", "RRRR GGGG OOOO BBBB WWWW YYYY")) ;
+//        System.out.println(getCubeSQL("Jha")) ;
+        System.out.println(setCubeSQL("Shinde", "Coochie", "rrrr")) ;
+        System.out.println(getStoreSQL("Shinde"));
     }
 
 }
