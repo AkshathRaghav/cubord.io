c50701718c7b087cddefd39cc3d85ed7a7a28382
Akshath Raghav <akshathraghav.r@gmail.com>
2021-06-21 16:28:40 +0530

    Add files via upload

---
+++ src/Timing.java
@@ -0,0 +1,56 @@
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import java.util.* ;
+import java.awt.event.*;
+import javax.swing.Timer;
+import static java.lang.Math.abs;
+
+public class Timing extends ListenerAdapter  {
+    Dictionary<String, Long> times = new Hashtable<String, Long>();
+    Dictionary<String, Boolean> timestart = new Hashtable<>();
+    ArrayList<String> timerlist = new ArrayList<>() ;
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event){
+        String[] arg = event.getMessage().getContentRaw().split(" ");
+        if  ( arg.length == 2 && (arg[0] + arg[1]).equalsIgnoreCase(Bot.prefix + "s") ) {
+            times.put(Objects.requireNonNull(event.getMember()).getEffectiveName(), System.currentTimeMillis()) ;
+            timestart.put(Objects.requireNonNull(event.getMember()).getEffectiveName(), true) ;
+
+        }
+        else if  ( arg.length == 2 && (arg[0] + arg[1]).equalsIgnoreCase(Bot.prefix + "d") ) {
+            String n = Objects.requireNonNull(event.getMember()).getEffectiveName() ;
+            if (timestart.get(n) == null) {
+                event.getChannel().sendMessage("Hey " + event.getMember().getAsMention() + ", start the stopwatch first").queue();
+            }
+            else {
+                double time = ((double) System.currentTimeMillis() - times.get(n)) / 1000;
+                timestart.put(n, false);
+                EmbedBuilder info = new EmbedBuilder();
+                info.setColor(0Xa80d2c);
+                info.addField(n + "'s time : ", String.valueOf(abs(time)) + "seconds", true);
+
+                timestart.remove(n);
+                times.remove(n) ;
+                System.out.println(timestart) ;
+
+
+                event.getChannel().sendMessage(info.build()).queue();
+                info.clear();
+            }
+
+        }
+        else if ( arg.length == 3 && (arg[0] + arg[1]).equalsIgnoreCase(Bot.prefix + "-t")  ) {
+            timerlist.add(Objects.requireNonNull(event.getMember()).getEffectiveName()) ;
+            ActionListener taskPerformer = new ActionListener() {
+                public void actionPerformed(ActionEvent evt) {
+                    event.getChannel().sendMessage("Timer's up, " + timerlist.remove(0)).queue();
+                }
+            };
+            Timer timer = new Timer(Integer.parseInt(String.valueOf(arg[2].charAt(arg[2].length()-1))) * 1000, taskPerformer);
+            timer.setRepeats(false);
+            timer.start();
+
+        }
+    }
+
+}
