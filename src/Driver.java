7735455f2c6f2d556813861cec64a95ba314031f
Akshath Raghav <akshathraghav.r@gmail.com>
2021-06-25 19:35:27 +0530

    Delete src directory

--- src/Driver.java
+++
@@ -1,8 +0,0 @@
-public class Driver {
-    public static void main(String[] args) {
-        String[] temp =  {"RRRRRRRRR", "GGGGGGGGG", "OOOOOOOOO", "BBBBBBBBB", "WWWWWWWWW", "YYYYYYYYY"};
-        Cube3d cube = new Cube3d(temp)  ;
-//        System.out.println(cube.isSolved());
-        System.out.println(cube.toarr());
-    }
-}
