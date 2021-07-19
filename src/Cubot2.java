f72888991ba00bff6b06e8516d7ecc0718beb2bb
Akshath Raghav <akshathraghav.r@gmail.com>
2021-07-19 19:21:05 +0530

    Update Cubot2.java

--- src/Cubot2.java
+++ src/Cubot2.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 
-public class Cubot2d extends Checker2d{
+public class Cubot2 extends Checker2 {
 
-    public Cubot2d(String[] s) throws IllegalArgumentException { super(s) ;}
+    public Cubot2(String[] s) throws IllegalArgumentException { super(s) ;}
     public boolean isSolved() { return super.isSolved() ;}
     public String toString() {
         return cubie.toString() ;
@@ -12,12 +12,12 @@ public class Cubot2d extends Checker2d{
     }
     public String[][][][] getSolvedCube() { return super.getSolvedCube() ; }
     public ArrayList<String> compareToSolved()  { return super.compareToSolved() ; }
-    public String stringAlg(String s) { return super.stringalg(s) ; }
+    public String stringAlg(String s) { return super.stringAlg(s) ; }
     public String getScramble(int n, boolean check) { return super.getScramble(n, check) ; }
-    public String reverseAlg(String s, boolean check ) { return  super.reversealg(s, check ) ; }
+    public String reverseAlg(String s, boolean check ) { return  super.reverseAlg(s, check ) ; }
     public String indexString() { return super.indexString() ; }
+    public String solve() { return super.solve() ; }
     public String toarr() { return super.toarr() ; }
-
-
+    public boolean isValid() { return super.isValid();  }
 }
 
